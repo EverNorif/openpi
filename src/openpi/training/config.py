8cf5c9edc48f7d9fe75aa63388b3aa5d60e99d6e
EverNorif <1320346985@qq.com>
2025-09-09 18:51:44 +0800

    add lora finetune config.

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -812,6 +812,18 @@ _CONFIGS = [
         num_train_steps=30_000,
     ),
     TrainConfig(
+        name="pi0_lora_so101_pick_orange",
+        model=pi0_config.Pi0Config(paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
+        data=LeRobotSO101DataConfig(
+            repo_id="EverNorif/leisaac-pick-orange",
+            base_config=DataConfig(prompt_from_task=True),
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi0_base/params"),
+        num_train_steps=30_000,
+        freeze_filter=pi0_config.Pi0Config(paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora").get_freeze_filter(),
+        ema_decay=None,
+    ),
+    TrainConfig(
         name="pi0_fast_so101_pick_orange",
         model=pi0_fast.Pi0FASTConfig(action_dim=6, action_horizon=10, max_token_len=180),
         data=LeRobotSO101DataConfig(
@@ -822,23 +834,40 @@ _CONFIGS = [
         num_train_steps=30_000,
     ),
     TrainConfig(
-        name="pi05_so101_pick_orange",
-        model=pi0_config.Pi0Config(pi05=True, action_horizon=10, discrete_state_input=False),
+        name="pi0_fast_lora_so101_pick_orange",
+        model=pi0_fast.Pi0FASTConfig(action_dim=6, action_horizon=10, max_token_len=180, paligemma_variant="gemma_2b_lora"),
         data=LeRobotSO101DataConfig(
             repo_id="EverNorif/leisaac-pick-orange",
             base_config=DataConfig(prompt_from_task=True),
         ),
-        lr_schedule=_optimizer.CosineDecaySchedule(
-            warmup_steps=10_000,
-            peak_lr=5e-5,
-            decay_steps=1_000_000,
-            decay_lr=5e-5,
-        ),
-        optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
-        ema_decay=0.999,
-        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
-        pytorch_weight_path="/path/to/your/pytorch_weight_path",
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi0_fast_base/params"),
         num_train_steps=30_000,
+        freeze_filter=pi0_fast.Pi0FASTConfig(action_dim=6, action_horizon=10, max_token_len=180, paligemma_variant="gemma_2b_lora").get_freeze_filter(),
+        ema_decay=None,
+    ),
+    TrainConfig(
+        name="pi05_so101_pick_orange",
+        model=pi0_config.Pi0Config(pi05=True, action_horizon=10),
+        data=LeRobotSO101DataConfig(
+            repo_id="EverNorif/leisaac-pick-orange",
+            base_config=DataConfig(prompt_from_task=True),
+        ),
+        batch_size=128,
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
+        num_train_steps=30_000,
+    ),
+    TrainConfig(
+        name="pi05_lora_so101_pick_orange",
+        model=pi0_config.Pi0Config(pi05=True, action_horizon=10, paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
+        data=LeRobotSO101DataConfig(
+            repo_id="EverNorif/leisaac-pick-orange",
+            base_config=DataConfig(prompt_from_task=True),
+        ),
+        batch_size=128,
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
+        num_train_steps=30_000,
+        freeze_filter=pi0_config.Pi0Config(pi05=True, action_horizon=10, paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora").get_freeze_filter(),
+        ema_decay=None,
     ),
     #
     # Fine-tuning Aloha configs.
