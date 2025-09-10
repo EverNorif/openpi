532e80ffca923d612be829d1fb7329c1f014725f
EverNorif <1320346985@qq.com>
2025-09-10 18:08:22 +0800

    lwlab so101 pick orange config.

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -402,6 +402,51 @@ class LeRobotSO101DataConfig(DataConfigFactory):
 
 
 @dataclasses.dataclass(frozen=True)
+class LeRobotSO101LWLabDataConfig(DataConfigFactory):
+    action_sequence_keys: Sequence[str] = ("action",)
+
+    @override
+    def create(self, assets_dirs: pathlib.Path, model_config: _model.BaseModelConfig) -> DataConfig:
+        # policy-dataset_keys(used for data:dict in so101_policy) -> inference-keys (lerobot-dataset keys) need for policy server
+        repack_transform = _transforms.Group(
+            inputs=[
+                _transforms.RepackTransform(
+                    {
+                        "images/front": "observation.images.agentview_all",
+                        "images/wrist": "observation.images.agentview_hand",
+                        "state": "observation.state",
+                        "actions": "action",
+                        "prompt": "prompt",
+                    }
+                )
+            ]
+        )
+
+        data_transforms = _transforms.Group(
+            inputs=[so101_policy.SO101Inputs(model_type=model_config.model_type)],
+            outputs=[so101_policy.SO101Outputs()],
+        )
+
+        # convert absolute actions to delta actions, except for the gripper action
+        # the delta action transform
+        delta_action_mask = _transforms.make_bool_mask(5, -1)
+        data_transforms = data_transforms.push(
+            inputs=[_transforms.DeltaActions(delta_action_mask)],
+            outputs=[_transforms.AbsoluteActions(delta_action_mask)],
+        )
+
+        model_transforms = ModelTransformFactory()(model_config)
+
+        return dataclasses.replace(
+            self.create_base_config(assets_dirs, model_config),
+            repack_transforms=repack_transform,
+            data_transforms=data_transforms,
+            model_transforms=model_transforms,
+            action_sequence_keys=self.action_sequence_keys,
+        )
+
+
+@dataclasses.dataclass(frozen=True)
 class LeRobotG1DataConfig(DataConfigFactory):
     action_sequence_keys: Sequence[str] = ("action",)
 
@@ -910,6 +955,33 @@ _CONFIGS = [
         model=pi0_config.Pi0Config(pi05=True, action_horizon=10, paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
         data=LeRobotSO101DataConfig(
             repo_id="EverNorif/leisaac-pick-orange",
+            base_config=DataConfig(prompt_from_task=True),
+        ),
+        batch_size=8, # used in 24GB RAM
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
+        num_train_steps=30_000,
+        freeze_filter=pi0_config.Pi0Config(pi05=True, action_dim=6, action_horizon=10, paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora").get_freeze_filter(),
+        ema_decay=None,
+    ),
+    #
+    # Fine-tuning LWLab SO101 configs.
+    #
+    TrainConfig(
+        name="pi05_lwlab_so101_pick_orange",
+        model=pi0_config.Pi0Config(pi05=True, action_horizon=10),
+        data=LeRobotSO101LWLabDataConfig(
+            repo_id="EverNorif/lwlab_so101_pick_orange",
+            base_config=DataConfig(prompt_from_task=True),
+        ),
+        batch_size=128,
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
+        num_train_steps=30_000,
+    ),
+    TrainConfig(
+        name="pi05_lora_lwlab_so101_pick_orange",
+        model=pi0_config.Pi0Config(pi05=True, action_horizon=10, paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
+        data=LeRobotSO101LWLabDataConfig(
+            repo_id="EverNorif/lwlab_so101_pick_orange",
             base_config=DataConfig(prompt_from_task=True),
         ),
         batch_size=8, # used in 24GB RAM
