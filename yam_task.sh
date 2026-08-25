feed92d4c701730aac8d2bf829a4e08928c0db04
EverNorif <1320346985@qq.com>
2026-08-25 06:00:02 +0000

    rebuild env in pro6000.

--- yam_task.sh
+++ yam_task.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-CUDA_VISIBLE_DEVICES=7 uv run scripts/compute_norm_stats.py --config-name pi05_yam_sortitem
+python scripts/compute_norm_stats.py --config-name pi05_yam_sortitem
 
-CUDA_VISIBLE_DEVICES=7 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi05_yam_sortitem --exp-name pi05_yam_sortitem_abs_v0
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 python scripts/train.py pi05_yam_sortitem --exp-name pi05_yam_sortitem_abs_v0
